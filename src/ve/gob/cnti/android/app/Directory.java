8bc1007829e78ab8e189703ece59e389e7dd66d7
EhisonBP <ehisonbp@gmail.com>
2013-04-22 10:56:11 -0430

    Modificacion en interfaz de Directorio

--- src/ve/gob/cnti/android/app/Directory.java
+++ src/ve/gob/cnti/android/app/Directory.java
@@ -1,13 +1,24 @@
 package ve.gob.cnti.android.app;
 
 import ve.gob.cnti.android.R;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class Directory extends Main {
+public class Directory extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.directory);
+	}
+
+	/**
+	 * Metodo para terminar la actividad
+	 * 
+	 * @param button
+	 */
+	public void onMainClick(View button) {
+		finish();
 	}
 }
